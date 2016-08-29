e50bf1f0e002ff11a9f873f30584bf401fcaed36
Debbie Milburn <debbiemilburn2@gmail.com>
2016-08-29 14:48:19 -0400

    Add description to salamanders

--- app/models/salamander.rb
+++ app/models/salamander.rb
@@ -1,4 +1,5 @@
 class Salamander < ActiveRecord::Base
   validates :name, presence: true
+  validates :description, presence: true
 
 end
--- db/migrate/20160824092101_create_salamanders.rb
+++ db/migrate/20160824092101_create_salamanders.rb
@@ -2,6 +2,7 @@ class CreateSalamanders < ActiveRecord::Migration
   def change
     create_table :salamanders do |t|
       t.string :name, null: false
+      t.string :description, null: false
 
       t.timestamps null: false
     end
